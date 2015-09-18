92c7ef2d548f5123036a2d9566466e35dffee716
Amy Wang <amy@0xdata.com>
2015-09-18 14:03:27 -0700

    test previous bugs of names and colnames not working

--- h2o-r/tests/testdir_misc/runit_headers.R
+++ h2o-r/tests/testdir_misc/runit_headers.R
@@ -2,13 +2,22 @@ setwd(normalizePath(dirname(R.utils::commandArgs(asValues=TRUE)$"f")))
 source('../h2o-runit.R')
 
 test.headers <- function() {
-
+  
+  # Test to make sure headers are replaced correctly
   headers <- h2o.importFile(locate("smalldata/airlines/allyears2k_headers_only.csv"), destination_frame = "airlines_headers")
   hex <- h2o.importFile(locate("smalldata/airlines/allyears2k.zip"), col.names=headers, destination_frame = "airlines")
   print(names(headers))
   print(names(hex))
   checkIdentical(names(headers), names(hex))
 
+  # Test to make sure files that go through parseRaw gets proper headers
+  iris.raw <- h2o.importFile( path = locate("smalldata/iris/iris.csv"), parse = F)
+  iris.hex <- h2o.parseRaw( data = iris.raw, destination_frame = "iris.hex",
+                            col.types = c("numeric", "numeric", "numeric", "numeric", "enum")) 
+  print(names(iris.hex))
+  print(paste0("C", 1:5))
+  checkIdentical(names(iris.hex), paste0("C", 1:5))
+  
   testEnd()
 }
 
